8dae25527b18d3190e9487432d748a152008a92f
Hunter Kreshock <hkreshock@gmail.com>
2019-02-21 10:39:40 -0700

    server running

--- src/app.js
+++ src/app.js
@@ -8,16 +8,15 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use((req, res, next) => {
-
+  next();
 });
 
-app.get('./movies', (req, res) => {
-
-  const genre = req.query;
+app.get('/movies', (req, res) => {
+  const genre = req.query.genre;
   const country = req.query;
   const avg_vote = req.query;
-
-  console.log(genre, country, avg_vote);
+  res.send(req.query);
+  console.log(genre);
 });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,16 +1,6 @@
 'use strict';
 
-const express = require('express');
-const morgan = require('morgan');
-
-const app = express();
-
-app.use(morgan('dev'));
-
-app.use((req, res) => {
-  res.send('Hello, world!');
-});
-
+const app = require('./app');
 const PORT = 8000;
 
 app.listen(PORT, () => {
